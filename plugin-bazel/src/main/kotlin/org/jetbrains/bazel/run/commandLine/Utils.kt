b0ba2b76dc1583cae91a0a3e9f7aaf28be0922db
Olga Borzenkova <kyssike@gmail.com>
2025-12-16 11:39:57 +0300

    propagate testExecutableArguments

--- plugin-bazel/src/main/kotlin/org/jetbrains/bazel/run/commandLine/Utils.kt
+++ plugin-bazel/src/main/kotlin/org/jetbrains/bazel/run/commandLine/Utils.kt
@@ -3,3 +3,5 @@ package org.jetbrains.bazel.run.commandLine
 import org.jetbrains.bazel.commons.toProgramArguments
 
 fun transformProgramArguments(input: String?): List<String> = input?.toProgramArguments() ?: emptyList()
+
+fun transformProgramArguments(input: List<String>?): String? = input?.joinToString(separator = " ")
